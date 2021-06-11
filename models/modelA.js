5a7c548695b9bd408a0cff212595062b32922cc1
Emily Corcoran <cathleencorcoran@gmail.com>
2021-06-11 19:48:46 -0400

    Initial commit

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaurObject = {
+  name: "Tom the T-Rex",
+  favoriteFood: "Candy Corn",
+  age: 75,
+  peopleEaten: 0,
+  eatsPeople: false,
+  jurrasicPeriod: true,
+  friends: ["Raptor", "Stegosaurus", "Larry"],
+
+}
