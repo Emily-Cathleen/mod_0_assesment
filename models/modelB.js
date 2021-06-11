505346036a66d8d6406a574780eb83a03dd86d02
Emily Corcoran <cathleencorcoran@gmail.com>
2021-06-11 19:59:15 -0400

    Initial Commit

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var developerObject = {
+  nameFirst: "Emily",
+  nameLast: "Corcoran",
+  age: 30,
+  daysAwake: 2,
+  isVeggitarian: true,
+  isAngry: false,
+  activities: ["yoga", "school", "running"],
+  leastFavoriteChores: ["dishes", "laundry", "going to work"]
+   
+
+}
