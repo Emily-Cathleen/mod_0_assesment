46809138eed96cf216e1961af85e156592628a3f
Emily Corcoran <cathleencorcoran@gmail.com>
2021-06-11 19:30:05 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var Movie = "Toy Story";
+var isEntertaining = true;
+Movie = "Scar Face";
+var num4 = 4;
+var num6 = 2;
+var product = num4 * num6;
+var favoriteFoods = ["Banana", "Taco", "Candy"];
+favoriteFoods.shift ("Banana");
+var MovieLength = Movie.length; // prints how many characters in movie
